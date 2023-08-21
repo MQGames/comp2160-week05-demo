d847a6c849642b093ba9e94cba97e7d736649424
Malcolm Ryan <malcolm.ryan@gmail.com>
2023-08-21 19:01:11 +1000

    Shooting lasers

--- COMP2160 Week 5 Demo/Assets/Scripts/PlayerControl.cs
+++ COMP2160 Week 5 Demo/Assets/Scripts/PlayerControl.cs
@@ -7,9 +7,22 @@ public class PlayerControl : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5;
-
     [SerializeField]
     private float turnSpeed = 5;
+    [SerializeField]
+    private float laserMaxDistance = 5;
+    [SerializeField]
+    private float laserSpeed = 50;
+    [SerializeField]
+    private LayerMask enemyLayerMask;
+    [SerializeField]
+    private ParticleSystem explosionPrefab;
+
+    private bool laserFiring = false;
+    private bool laserEnding = false;
+    private float laserDistance = 0;
+    private float laserHitDistance = 0;
+    private GameObject laserTarget;
 
     private Actions actions;
     private InputAction forwardAction;
@@ -47,9 +60,67 @@ public class PlayerControl : MonoBehaviour
         pos.y = (pos.y + 1) % 1;
         transform.position = Camera.main.ViewportToWorldPoint(pos);
 
-        if (shootAction.WasPressedThisFrame()) 
+        if (laserEnding)
         {
+            laserDistance += laserSpeed * Time.deltaTime;
+            if (laserDistance >= laserHitDistance) 
+            {
+                laserDistance = laserHitDistance;
+                laserEnding = false;
+            }
 
+            Vector3 start = transform.TransformPoint(laserDistance * Vector3.up);
+            Vector3 end = transform.TransformPoint(laserHitDistance * Vector3.up);
+            var lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, start);
+            lineRenderer.SetPosition(1, end);
+        }
+
+        if (laserFiring)
+        {
+            laserDistance += laserSpeed * Time.deltaTime;
+            if (laserDistance >= laserHitDistance)
+            {
+                laserDistance = 0;
+                laserFiring = false;
+                laserEnding = true;
+                
+                if (laserTarget != null)
+                {
+                    var particles = Instantiate(explosionPrefab);
+                    particles.transform.position = laserTarget.transform.position;
+                    Destroy(laserTarget);
+                }
+            }
+
+            Vector3 start = transform.TransformPoint(0 * Vector3.up);
+            Vector3 end = transform.TransformPoint(laserDistance * Vector3.up);
+            var lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, start);
+            lineRenderer.SetPosition(1, end);
+        }
+        
+        if (shootAction.WasPressedThisFrame() && !laserFiring && !laserEnding) 
+        {
+            laserFiring = true;
+            laserDistance = 0;
+
+            Vector2 origin = transform.position;
+            Vector2 direction = transform.up;
+
+            var hit = Physics2D.Raycast(origin, direction, laserMaxDistance, enemyLayerMask); 
+            if (hit.collider == null)
+            {                
+                Debug.Log("hit = null");
+                laserHitDistance = laserMaxDistance;
+                laserTarget = null;
+            }
+            else
+            {
+                Debug.Log($"hit = {hit.collider.gameObject.name}");
+                laserHitDistance = hit.distance;
+                laserTarget = hit.collider.gameObject;
+            }
         }
     }
 }
